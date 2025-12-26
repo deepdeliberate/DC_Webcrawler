062b6c042ff809bf9d35a53cef5e5701114cba25
Naman Deep <namandeep444@gmail.com>
2025-12-26 06:55:08 +0530

    Fixed content parsing bug

--- crawler/parser.py
+++ crawler/parser.py
@@ -3,14 +3,16 @@ from bs4 import BeautifulSoup
 def parse_wiki_page(html):
     soup = BeautifulSoup(html, "lxml")
     title_tag = soup.find("h1", id = "firstHeading")
-    content_div = soup.find("div", id="mw-context-text")
+    content_div = soup.find("div", id="mw-content-text")
 
     title = title_tag.text.strip() if title_tag else "No Title"
     summary = ""
     if content_div:
-        p = content_div.find("p")
-        if p:
-            summary = p.text.strip()
+        for p in content_div.find_all("p"):
+            text = p.get_text(strip=True)
+            if text:
+                summary = text
+                break
     
     return {
         "title": title,
